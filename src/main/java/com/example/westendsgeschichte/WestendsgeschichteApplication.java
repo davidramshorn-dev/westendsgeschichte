65be7c27196288b79bf8d1e23345bc5e3a5bb251
davidramshorn-dev <david.ramshorn@gmail.com>
2026-04-20 10:33:37 +0200

    Initial commit

---
+++ src/main/java/com/example/westendsgeschichte/WestendsgeschichteApplication.java
@@ -0,0 +1,13 @@
+package com.example.westendsgeschichte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestendsgeschichteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestendsgeschichteApplication.class, args);
+	}
+
+}
