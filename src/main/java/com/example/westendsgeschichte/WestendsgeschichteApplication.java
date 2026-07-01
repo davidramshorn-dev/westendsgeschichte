9dd4e667248be3e597670ba63986a3bb2bbac5b9
davidramshorn-dev <david.ramshorn@gmail.com>
2026-07-01 09:36:04 +0200

    ordnerstruktur verbessert

--- src/main/java/com/example/westendsgeschichte/WestendsgeschichteApplication.java
+++ src/main/java/com/example/westendsgeschichte/WestendsgeschichteApplication.java
@@ -1,4 +1,4 @@
-package com.example.westendsgeschichte;
+package com.westendsgeschichte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
